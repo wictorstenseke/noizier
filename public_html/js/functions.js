e1e7febc23ec4a89e3086b1411ebe347874281b0
wictorstenseke <wictor.johansson@gmail.com>
2016-09-13 17:53:45 +0200

    added jQuery and Javascript to animate scroll

---
+++ public_html/js/functions.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    // Scroll to show the link
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing');
+  });
+});
